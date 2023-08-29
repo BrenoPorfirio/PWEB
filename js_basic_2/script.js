26fb83756ea4ab4d0d1b16ef43ed97c321e5c6d4
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-08-28 23:35:12 -0300

    add function shuffle

--- js_basic_2/script.js
+++ js_basic_2/script.js
@@ -15,8 +15,23 @@ function sortCervejas() {
   loadTable()
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+function shuffleCervejas() {
+  shuffle(cervejas)
+  loadTable()
+}
+
 let botaoCarregar = document.getElementById("botaoCarregar")
 botaoCarregar.addEventListener("click", loadTable)
 
 let botaoOrdenar = document.getElementById("BSort")
 botaoOrdenar.addEventListener("click", sortCervejas)
+
+let botaoEmbaralhar = document.getElementById("BShuffle")
+botaoEmbaralhar.addEventListener("click", shuffleCervejas)
