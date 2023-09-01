ac9f8437f29c147f7d1e87fbc0b20f4f0d33ad2a
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-08-31 22:42:41 -0300

    step 2

--- js_arrow_and_callback/script.js
+++ js_arrow_and_callback/script.js
@@ -1,15 +1,43 @@
+let games = [
+  "Red Dead Redemption",
+  "CS-GO",
+  "Deep Rocket Galatic",
+  "Forza Horizon",
+  "Left 4 Dead 2",
+]
+
 const transformar = (item) => {
-  return `<h1>${item}</h1>`
+  return `<h1 class="nome-jogo">${item}</h1>`
 }
 
-const carregarDiv = (cervs) => {
+const sort = () => {
+  games.sort()
+  carregarDiv()
+}
+
+const carregarDiv = () => {
   const div = document.getElementById("meuAmor")
-  const cervejasHtml = cervs.map(transformar)
-  div.innerHTML = `${cervejasHtml.join("\n")}`
+  const gamesHtml = games.map(transformar)
+  div.innerHTML = `${gamesHtml.join("\n")}`
 }
 
-let botao = document.getElementById("botaoCarregar")
-botao.addEventListener("click", () => {
-  const cervejas = ["Guiness", "Desperados", "Becks"]
-  carregarDiv(cervejas)
-})
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+function shuffleGames() {
+  shuffle(games)
+  carregarDiv()
+}
+
+let botaoCarregar = document.getElementById("botaoCarregar")
+botaoCarregar.addEventListener("click", carregarDiv)
+
+let botaoOrdenar = document.getElementById("botaoOrdenar")
+botaoOrdenar.addEventListener("click", sort)
+
+let botaoEmbaralhar = document.getElementById("botaoEmbaralhar")
+botaoEmbaralhar.addEventListener("click", shuffleGames)
