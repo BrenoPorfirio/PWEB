0e2bafb90f81c9f8ed41f6e2c72b79ea8a5fbb4d
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-22 22:42:29 -0300

    add folder to recipe 14

---
+++ React+NextJS/pages/recipe14/movies.js
@@ -0,0 +1,23 @@
+export default function Movies({ data }) {
+  return (
+    <div>
+      <div>
+        {data.Search.map((m) => (
+          <div>
+            {m.Title} --- {m.Year}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`http://www.omdbapi.com/?apikey=ca98445&s=bagdad`)
+  const data = await res.json()
+  return {
+    props: {
+      data,
+    },
+  }
+}
